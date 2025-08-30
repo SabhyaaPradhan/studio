12108aad0e4846451a9e955350a410abfc20622f
Sabhyaa Pradhan <sabhyaapradhan2005@gmail.com>
2025-08-30 14:22:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/landing/features.tsx
+++ src/components/landing/features.tsx
@@ -26,7 +26,7 @@ const features = [
   },
   {
     name: "Auto-generated tags",
-    image: "/images/auto-generated-tags.png",
+    image: "https://images.unsplash.com/photo-1654764450232-d0724be6dcd7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyMHx8Ym90fGVufDB8fHx8MTc1NjU2MzY3MHww&ixlib=rb-4.1.0&q=80&w=1080",
     hint: "tagging system",
   },
   {
