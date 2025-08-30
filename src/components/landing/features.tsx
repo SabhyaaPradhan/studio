e673c8859548d4dfed99914db3b6dd9c9936c2a2
Sabhyaa Pradhan <sabhyaapradhan2005@gmail.com>
2025-08-30 14:35:28 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="d

--- src/components/landing/features.tsx
+++ src/components/landing/features.tsx
@@ -26,13 +26,8 @@ const features = [
   },
   {
     name: "Auto-generated tags",
-    image: "/images/auto-generated-tags.png",
+    image: "https://images.unsplash.com/photo-1554497103-605884594625?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHx0YWdnaW5nJTIwc3lzdGVtfGVufDB8fHx8MTc1NjU2MzgyNnww&ixlib=rb-4.1.0&q=80&w=1080",
     hint: "tagging system",
-  },
-  {
-    name: "Intelligent ticket routing",
-    image: "/images/intelligent-ticket-routing.png",
-    hint: "ticket system",
   },
   {
     name: "Anomaly notifications",
